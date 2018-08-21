543282ff435d4ba8f1e2c0a737adcc9cf95a2aeb
Riccardo Tommasini <tomma156@gmail.com>
2018-08-21 14:46:20 +0200

    removed the stream item idea

--- src/main/java/it/polimi/jasper/engine/rsp/streams/RegisteredEPLStream.java
+++ src/main/java/it/polimi/jasper/engine/rsp/streams/RegisteredEPLStream.java
@@ -1,33 +1,24 @@
 package it.polimi.jasper.engine.rsp.streams;
 
 import com.espertech.esper.client.EPStatement;
-import it.polimi.yasper.core.spe.windowing.assigner.WindowAssigner;
 import it.polimi.yasper.core.stream.Stream;
 import it.polimi.yasper.core.stream.rdf.RegisteredRDFStream;
 import lombok.Getter;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.jena.graph.Graph;
 
 /**
  * Created by riccardo on 10/07/2017.
  */
 @Getter
-public class RegisteredEPLStream extends RegisteredRDFStream {
+public class RegisteredEPLStream extends RegisteredRDFStream<Graph> {
 
     protected Stream stream;
     protected EPStatement e;
 
-    protected final List<WindowAssigner> assigners = new ArrayList<>();
-
     public RegisteredEPLStream(String uri, Stream s, EPStatement epl) {
         super(uri);
         this.stream = s;
-    }
-
-    @Override
-    public void addWindowAssiger(WindowAssigner windowAssigner) {
-        assigners.add(windowAssigner);
+        this.e = epl;
     }
 
 }
