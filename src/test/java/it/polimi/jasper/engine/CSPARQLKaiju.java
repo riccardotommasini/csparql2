0f7283f445eed42a46c27a5a8852014d353a94e9
mario_scrock <mario.scrocca@gmail.com>
2018-09-08 19:21:26 +0200

    Add test example parsing tracing data in JSON-LD through Kaiju WebSocket

---
+++ src/test/java/it/polimi/jasper/engine/CSPARQLKaiju.java
@@ -0,0 +1,56 @@
+package it.polimi.jasper.engine;
+
+import it.polimi.jasper.streams.RegisteredEPLStream;
+import it.polimi.yasper.core.engine.EngineConfiguration;
+import it.polimi.yasper.core.spe.operators.r2r.ContinuousQuery;
+import it.polimi.yasper.core.spe.operators.r2r.QueryConfiguration;
+import it.polimi.yasper.core.spe.operators.r2r.execution.ContinuousQueryExecution;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by Riccardo on 03/08/16.
+ */
+public class CSPARQLKaiju {
+
+    static Jasper sr;
+
+    public static void main(String[] args) throws InterruptedException, IOException, ConfigurationException {
+
+        URL resource = CSPARQLKaiju.class.getResource("/csparqlKaiju.properties");
+        QueryConfiguration config = new QueryConfiguration(resource.getPath());
+        EngineConfiguration ec = EngineConfiguration.loadConfig("/csparqlKaiju.properties");
+
+        sr = new Jasper(0, ec);
+
+        GraphStreamKaiju writer = new GraphStreamKaiju("http://streamreasoning.org/jasper/streams/stream2");
+
+        RegisteredEPLStream register = sr.register(writer);
+
+        writer.setWritable(register);
+
+        ContinuousQueryExecution cqe = sr.register(getQuery(".rspql"), config);
+
+        ContinuousQuery q2 = cqe.getContinuousQuery();
+
+        System.out.println(q2.toString());
+
+        System.out.println("<<------>>");
+
+        //In real application we do not have to start the stream.
+        (new Thread(writer)).start();
+
+    }
+
+    public static String getQuery(String suffix) throws IOException {
+        URL resource = CSPARQLKaiju.class.getResource("/q42" + suffix);
+        System.out.println(resource.getPath());
+        File file = new File(resource.getPath());
+        return FileUtils.readFileToString(file);
+    }
+
+}
